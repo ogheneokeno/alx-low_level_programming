3a2bde206fa6ed9ed8e312aa2fe0b40f94a684fe
Gideon Elila <giftelila@gmail.com>
2022-12-12 14:04:56 -0800

    mandateroy task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints alpha in lower and upper case
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
