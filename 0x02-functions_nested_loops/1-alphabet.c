9f5e4b47114b013805b30297a8de6de501a52bcf
Gideon Elila <giftelila@gmail.com>
2022-12-13 14:19:49 -0800

    task 2%3

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * print - prints the alphabet
- *
+ * prints_alpahbets - prints the alpha in lowercase
+ * Returns: On success 1
  */
 void print_alphabet(void)
 {
 	char la;
 
 	for (la = 'a'; la <= 'z'; la++)
+	{
 		_putchar(la);
+	}
 
 	_putchar('\n');
 }
