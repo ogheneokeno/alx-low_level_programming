3b84b6b938ceb4baed16ee7e8f30e4affc8e3c38
Gideon Elila <giftelila@gmail.com>
2022-12-19 12:01:53 -0800

    task 7 to 10

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#inculde "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: destination
+ * @src: source
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+
+	while (*(src + len) != '\0')
+	{
+		*(dest + len) = *(src + len);
+		lent++;
+	}
+
+	*(dest + len) = '\0';
+	return (dest);
+}
