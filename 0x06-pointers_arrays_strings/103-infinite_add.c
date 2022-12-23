288753d44ed076d476dd1f2be783f203d4426758
Gideon Elila <giftelila@gmail.com>
2022-12-22 19:00:53 -0800

    task 11-12

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,6 +5,7 @@ include "main.h"
  * @n: integer params
  * Return: 0
  */
+
 void rev_string(char *n)
 {
 	int i = 0;
@@ -26,6 +27,7 @@ void rev_string(char *n)
 		*(n + i) = temp;
 	}
 }
+
 /**
  * infinite_add - add 2 numbers together
  * @n1: text representation of 1st number to add
@@ -34,6 +36,7 @@ void rev_string(char *n)
  * @size_r: buffer size
  * Return: pointer to calling function
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
@@ -46,8 +49,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	while (*(n2 + j) != '\0')
 
 		j++;
-	i--;
-	j--;
+		i--;
+		j--;
 
 	if (j >= size_r || i >= size_r)
 
